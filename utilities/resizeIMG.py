342af3cfe886e8fb9c47bf903956ab6915b0d0ca
Jonas Boeno <boeno.jonas@gmail.com>
2022-05-24 17:20:07 -0300

    Corrigir nome dos diretório

--- utilities/resizeIMG.py
+++ utilities/resizeIMG.py
@@ -5,8 +5,8 @@ import os
 def resize():
     print('Resimensionando imagens!')
     tamanho = (1120, 620)
-    lista = [x for x in os.listdir('cams')]
+    lista = [x for x in os.listdir('images')]
     for i in lista:
         im = Image.open('cams\\' + i)
         im_menor = im.resize(tamanho)
-        im_menor.save('cams_resize\\' + i)
+        im_menor.save('resized_images\\' + i)
